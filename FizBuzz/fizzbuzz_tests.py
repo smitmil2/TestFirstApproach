a7870bbf2494431a0de8edce6eb807baa1d400b9
Miles Smith <miles@cerebralclutch.com>
2021-03-07 22:09:35 -0800

    Added test for buzz

--- FizBuzz/fizzbuzz_tests.py
+++ FizBuzz/fizzbuzz_tests.py
@@ -9,5 +9,10 @@ class TestCase(unittest.TestCase):
         result = fizzbuzz.is_fizz(21)
         self.assertTrue(result)
 
+    def testBuzz(self):
+        self.assertFalse(fizzbuzz.is_buzz(2))
+
+        self.assertTrue(fizzbuzz.is_buzz(5))
+        
 if __name__ == '__main__':
     unittest.main()
