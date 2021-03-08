7f10d4f34dfc539a3c9230dae3fa198d6ba8a078
Miles Smith <miles@cerebralclutch.com>
2021-03-07 22:12:48 -0800

    Wrote test for fizzbuzz

--- FizBuzz/fizzbuzz_tests.py
+++ FizBuzz/fizzbuzz_tests.py
@@ -13,6 +13,19 @@ class TestCase(unittest.TestCase):
         self.assertFalse(fizzbuzz.is_buzz(2))
 
         self.assertTrue(fizzbuzz.is_buzz(5))
+
+    def testFizzBuzz(self):
+        result = fizzbuzz.fizzbuzz(1)
+        self.assertEqual(result, 1)
+
+        result = fizzbuzz(3)
+        self.assertEqual(result, 'Fizz')
+
+        result = fizzbuzz(5)
+        self.assertEqual(result, 'Buzz')
+
+        result = fizzbuzz(15)
+        self.assertEqual(result, 'FizzBuzz')
         
 if __name__ == '__main__':
     unittest.main()
